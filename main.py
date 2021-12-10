50a6a80ef5b951839ccb8bf2931d646c0907e3e1
natalia.osnovenko <natalia.osnovenko@arixess.com>
2021-12-10 22:43:40 +0200

    added last task

--- main.py
+++ main.py
@@ -48,13 +48,11 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        self.grades = {}
-        self.avg = {}
 
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) and course in self.courses_attached:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -96,46 +94,134 @@ class Lector(Mentor):
         avg = "Average grade for lectures: {}".format(self.average_grade_for_lectures())
         return "{}\n{}\n{}".format(first_name, last_name, avg)
 
+    def average_mark_for_lecture_all_lector_within_course(self, course):
+        pass
+
 
 best_student = Student('Ruoy', 'Eman', 'woman')
-best_student.add_marks('Python', 7)
 best_student.add_marks('Python', 9)
 best_student.add_marks('Python', 9)
-best_student.add_marks('Git', 3)
-best_student.add_marks('Git', 5)
-best_student.add_marks('Git', 6)
+best_student.add_marks('Python', 9)
+best_student.add_marks('Python for advanced', 3)
+best_student.add_marks('Python for advanced', 5)
+best_student.add_marks('Python for advanced', 6)
 best_student.average_grade_for_h_w()
-best_student.courses_in_progress += ['Python', 'Git']
+best_student.courses_in_progress += ['Python', 'Python for advanced']
 best_student.finished_courses += ['Introduction to programming']
 print(best_student)
 print("\n")
 
+first_student = Student('Nikita', 'Gordon', 'man')
+first_student.add_marks('Python', 5)
+first_student.add_marks('Python', 6)
+first_student.add_marks('Python', 8)
+first_student.add_marks('C++', 6)
+first_student.add_marks('C++', 5)
+first_student.add_marks('C++', 4)
+first_student.average_grade_for_h_w()
+first_student.courses_in_progress += ['Python', 'C++']
+first_student.finished_courses += ['How to find good work']
+print(first_student)
+print("\n")
+
+
+third_student = Student('Liza', 'Vornitsa', 'woman')
+third_student.add_marks('JS', 6)
+third_student.add_marks('JS', 5)
+third_student.add_marks('JS', 4)
+third_student.add_marks('Python', 5)
+third_student.add_marks('Python', 6)
+third_student.add_marks('Python', 8)
+third_student.average_grade_for_h_w()
+third_student.courses_in_progress += ['JS', 'Python']
+third_student.finished_courses += ["Introduction to Python"]
+print(third_student)
+print("\n")
 
 cool_reviewer = Reviewer('Some', 'Buddy')
-print("#Cool reviewer#")
-print(cool_reviewer)
-cool_reviewer.courses_attached += ['Python']
-
+cool_reviewer.courses_attached += ['Python', 'Python for advanced']
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 8)
 cool_reviewer.rate_hw(best_student, 'Python', 4)
+cool_reviewer.rate_hw(best_student, 'Python for advanced', 10)
+cool_reviewer.rate_hw(best_student, 'Python for advanced', 8)
+cool_reviewer.rate_hw(best_student, 'Python for advanced', 4)
 print(best_student.grades)
+print(cool_reviewer)
 
-second_student = Student('Lola', 'Frimey', 'woman')
-second_student.courses_in_progress += ['Algoritmika']
+first_reviewer = Reviewer('Hannah', 'Curni')
+first_reviewer.courses_attached += ['Python', 'C++']
+first_reviewer.rate_hw(first_student, 'Python', 10)
+first_reviewer.rate_hw(first_student, 'Python', 8)
+first_reviewer.rate_hw(first_student, 'Python', 4)
+first_reviewer.rate_hw(first_student, 'C++', 10)
+first_reviewer.rate_hw(first_student, 'C++', 8)
+first_reviewer.rate_hw(first_student, 'C++', 4)
+print(first_student.grades)
+print(first_reviewer)
+
+second_reviewer = Reviewer('Yulia', 'Yaroslava')
+second_reviewer.courses_attached += ['JS']
+second_reviewer.rate_hw(third_student, 'JS', 10)
+second_reviewer.rate_hw(third_student, 'JS', 8)
+second_reviewer.rate_hw(third_student, 'JS', 4)
+print(third_student.grades)
+print(second_reviewer)
+
 
 cool_lector = Lector('Denis', 'Kyzin')
-cool_lector.courses_attached += ['Algoritmika']
-cool_lector.add_marks(second_student, 'Algoritmika', 7)
-cool_lector.add_marks(second_student, 'Algoritmika', 9)
-cool_lector.add_marks(second_student, 'Algoritmika', 5)
-cool_lector.add_marks(second_student, 'Algoritmika1', 7)
-cool_lector.add_marks(second_student, 'Algoritmika2', 9)
-cool_lector.add_marks(second_student, 'Algoritmika3', 5)
+cool_lector.courses_attached += ['Python', 'Python for advanced']
+cool_lector.add_marks(best_student, 'Python', 7)
+cool_lector.add_marks(best_student, 'Python', 9)
+cool_lector.add_marks(best_student, 'Python', 5)
+cool_lector.add_marks(best_student, 'Python for advanced', 7)
+cool_lector.add_marks(best_student, 'Python for advanced', 9)
+cool_lector.add_marks(best_student, 'Python for advanced', 5)
 cool_lector.average_grade_for_lectures()
 print(cool_lector)
+print("\n")
 
-# Реализуйте возможность сравнивать (через операторы сравнения) между собой лекторов по средней оценке за лекции
-# и студентов по средней оценке за домашние задания.
+first_lector = Lector('Andrey', 'Efrimov')
+first_lector.courses_attached += ['Python', 'C++']
+first_lector.add_marks(first_student, 'Python', 7)
+first_lector.add_marks(first_student, 'Python', 9)
+first_lector.add_marks(first_student, 'Python', 5)
+first_lector.add_marks(first_student, 'C++', 7)
+first_lector.add_marks(first_student, 'C++', 9)
+first_lector.add_marks(first_student, 'C++', 5)
+first_lector.average_grade_for_lectures()
+print(first_lector)
+print("\n")
+
+second_lector = Lector('Olga', 'Svirnova')
+second_lector.courses_attached += ['JS', 'Python']
+second_lector.add_marks(third_student, 'JS', 2)
+second_lector.add_marks(third_student, 'JS', 4)
+second_lector.add_marks(third_student, 'JS', 3)
+second_lector.add_marks(third_student, 'Python', 9)
+second_lector.add_marks(third_student, 'Python', 8)
+second_lector.add_marks(third_student, 'Python', 9)
+second_lector.average_grade_for_lectures()
+print(second_lector)
+
+#to compare student and lector
 print(best_student > cool_lector)
 
+
+def average_mark_by_students_within_course(list_students, course):
+    marks_by_course = []
+    for student in list_students:
+        marks_by_course += student.grades.get(course, [])
+    return sum(marks_by_course) / len(marks_by_course)
+
+
+print('\nAvg mark {} by course {} of all student'.format(average_mark_by_students_within_course([best_student, first_student, third_student], 'Python'), 'Python'))
+
+
+def average_mark_by_lectors_for_lectures_within_course(list_lectors, course):
+    marks_by_lectures = []
+    for lector in list_lectors:
+        marks_by_lectures += lector.grades.get(course, [])
+    return sum(marks_by_lectures) / len(marks_by_lectures)
+
+print('\nAvg mark {} by course {} of all lectures by lectors'.format(average_mark_by_lectors_for_lectures_within_course([cool_lector, first_lector, second_lector], 'Python'), 'Python'))
